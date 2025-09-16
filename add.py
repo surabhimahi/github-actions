3b0d6000bba833848def7cc1a184fdb32e620144
surabhimahi <surabhimahendran040@gmail.com>
2025-09-16 10:16:21 +0530

    add function

--- add.py
+++ add.py
@@ -1,2 +1,8 @@
-num1 = int(input())
-num2 = int(input())
+# app.py
+# This is a test commit
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
